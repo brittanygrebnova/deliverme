5828eb091eededef5e9a694adc2e77b8caba49a5
github username <github email address>
2019-01-23 10:00:17 -0500

    correct order#update and orders/show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,9 @@ class OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.update(order_params)
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to order_path(@order)
   end
 
   def destroy
