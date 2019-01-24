31d7b4a9af26467167a4b1fd7ca86ec31a0f8412
github username <github email address>
2019-01-24 14:35:02 -0500

    added logout link to landing page for vendor, changed redirect after sign in for vendor to vendor show

--- app/controllers/vendors/sessions_controller.rb
+++ app/controllers/vendors/sessions_controller.rb
@@ -4,7 +4,7 @@ class Vendors::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
-    new_vendor_path
+    vendor_path(current_vendor)
   end
   # GET /resource/sign_in
   # def new
